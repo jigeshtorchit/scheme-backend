45a6bb2fa67a7e8ba5dfa50d0609cae2321f7446
Tamilselvan1255 <tamilselvan0677@gmail.com>
2023-12-06 12:20:08 +0530

    return to original

--- controllers/filterFacility.js
+++ controllers/filterFacility.js
@@ -46,7 +46,7 @@ async function filterFacilities(req, res) {
                 .find(filter)
                 .skip(skip)
                 .limit(parsedLimit);
-            res.status(200).send("Filter data successfully shown:", data = {
+            res.status(200).send({
                 data: filteredFacilities,
                 totalCount: totalFilteredFacilities,
                 pageSize: parsedLimit,
