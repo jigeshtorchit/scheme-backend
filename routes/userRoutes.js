759f331fef8b346eccb397e4bfb910292489aec0
Tamilselvan1255 <tamilselvan0677@gmail.com>
2024-01-02 18:57:33 +0530

    unwanted codes removed in userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,9 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/userAdd', userAdd);
-// router.get('/schemeView',schemeView);
-// router.get('/:id', getSchemeById);
-// router.delete('/schemeDelete/:id', schemeDelete);
 router.patch('/userUpdate/:id', userUpdate); 
 
 module.exports = router;
