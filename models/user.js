26892b612934bd63b9fda814da10f6bffac07995
Tamilselvan1255 <tamilselvan0677@gmail.com>
2024-01-05 15:42:26 +0530

    old code

--- models/user.js
+++ models/user.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true},
-  email: { type: String, required: true},
-  phone: { type: String, required: true},
-  rating: { type: Number},
-  comments: { type: String},
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+}, {
+  timestamps: true
 });
 
 const User = mongoose.model('User', userSchema);
-
 
 module.exports = User;
--- routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const { userAdd, userUpdate} = require("../controllers/user");
-const express = require('express');
-const router = express.Router();
-
-router.post('/userAdd', userAdd);
-router.patch('/userUpdate/:id', userUpdate); 
-
-module.exports = router;
- 
